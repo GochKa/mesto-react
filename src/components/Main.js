a826f566d11e8c32d6307ffab4abd62337ec0603
GochKa <gocha.workout@gmail.com>
2022-04-21 00:28:52 +0300

    ,

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../index.css';
 import Card from "./Card";
-import { CurrentUserContext } from "./contexts/CurrentUserContext";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Main(props){
   
